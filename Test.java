dd265b5e8184b7141d9f3eaa8eae927aa4ab3e88
RohitcoolKashyap <rohit.cse18@chitkarauniversity.edu.in>
2021-01-29 18:37:23 +0530

    update

--- Test.java
+++ Test.java
@@ -1,39 +1,19 @@
-import java.util.Arrays;
-
 class Solution {
 
-    public int findTargetSumWays(int[] nums, int diff) {
-        int sum = 0;
-        sum = Arrays.stream(nums).sum();
-        int s1 = ((diff + sum) / 2);
-        int n = nums.length;
-
-        if (diff > sum) return 0;
-        if ((diff + sum) % 2 != 0) return 0;
-
-        return count_subsets_equal_to_diff(nums, n, s1);
-
+    public static void main(String[] arg) {
+        String s1 = "abc";
+        String s2 = "abcd";
+        int count = 0;
+        int t = lcs(s1, s2, s1.length(), s2.length(), count);
+        System.out.println(t);
     }
 
-    public static int count_subsets_equal_to_diff(int[] nums, int n, int diff) {
-        int t[][] = new int[n + 1][diff + 1];
-        for (int i = 0; i < n + 1; i++) {
-            for (int j = 0; j < diff + 1; j++) {
-                if (i == 0)
-                    t[i][j] = 0;
-                if (j == 0)
-                    t[i][j] = 1;
-            }
-        }
-
-        for (int i = 1; i < n + 1; i++) {
-            for (int j = 0; j < diff + 1; j++) {
-                if (nums[i - 1] <= j)
-                    t[i][j] = t[i - 1][j] + t[i - 1][j - nums[i - 1]];
-                else
-                    t[i][j] = t[i - 1][j];
-            }
-        }
-        return t[n][diff];
+    static int lcs(String s1, String s2, int n, int m, int count) {
+        if (n == 0 || m == 0) return count;
+        if (s1.charAt(n - 1) == s2.charAt(m - 1))
+            count = lcs(s1, s2, n - 1, m - 1, count + 1);
+        count = Math.max(count,
+                Math.max(lcs(s1, s2, n, m - 1, 0), lcs(s1, s2, n - 1, m, 0)));
+        return count;
     }
 }
--- dynamicProgramming/Longest_common_subsequence.java
+++ dynamicProgramming/Longest_common_subsequence.java
@@ -2,22 +2,24 @@ package dynamicProgramming;
 
 public class Longest_common_subsequence {
     public static void main(String[] arg) {
-        String s1 = "GeeksforGeeks";
-        String s2 = "GeeksQuiz";
+        String s1 = "heap";
+        String s2 = "pea";
         // dp = new int[s1.length() + 1][s2.length() + 1];
         // Arrays.stream(dp).forEach(i -> Arrays.fill(i, -1));
         //System.out.println(lcs(s1, s2, s1.length(), s2.length()));
         //System.out.println(lcss(s1, s2, s1.length(), s2.length()));
-        System.out.println(Longest_common_subPrint(s1, s2, s1.length(), s2.length()));
+        //System.out.println(Longest_common_subPrint(s1, s2, s1.length(), s2.length()));
+        // System.out.println(shortest_common_superSequence(s1, s2, s1.length(), s2.length()));
+        min_opertation_to_convert_string(s1, s2, s1.length(), s2.length());
     }
 
     //    public static int lcs(String s1, String s2, int n, int m) {
 //        if (n == 0 || m == 0) return 0;
 //
 //        if (s1.charAt(n - 1) == s2.charAt(m - 1))
-//            return 1 + lcb(s1, s2, n - 1, m - 1);
+//            return 1 + lcs(s1, s2, n - 1, m - 1);
 //        else
-//            return Math.max(lcb(s1, s2, n, m - 1), lcb(s1, s2, n - 1, m));
+//            return Math.max(lcs(s1, s2, n, m - 1), lcs(s1, s2, n - 1, m));
 //    }
 
     //    static int[][] dp;
@@ -33,29 +35,39 @@ public class Longest_common_subsequence {
 //            return dp[n][m] = Math.max(lcb(s1, s2, n, m - 1), lcb(s1, s2, n - 1, m));
 //    }
     // Top down approach
-//    public static int lcs(String s1, String s2, int n, int m) {
-//
-//        int[][] dp = new int[n + 1][m + 1];
-//        for (int i = 0; i < n + 1; i++) {
-//            for (int j = 0; j < m + 1; j++) {
-//                if (i == 0 || j == 0)
-//                    dp[i][j] = 0;
-//            }
-//
-//        }
-//        for (int i = 1; i < n + 1; i++) {
-//            for (int j = 1; j < m + 1; j++) {
-//
-//                if (s1.charAt(i - 1) == s2.charAt(j - 1))
-//                    dp[i][j] = 1 + dp[i - 1][j - 1];
-//                else
-//                    dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
-//            }
-//        }
-//        return dp[n][m];
-//    }
+    public static int lcs(String s1, String s2, int n, int m) {
 
-    // Longest Common Substring
+        int[][] dp = new int[n + 1][m + 1];
+        for (int i = 0; i < n + 1; i++) {
+            for (int j = 0; j < m + 1; j++) {
+                if (i == 0 || j == 0)
+                    dp[i][j] = 0;
+            }
+
+        }
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < m + 1; j++) {
+
+                if (s1.charAt(i - 1) == s2.charAt(j - 1))
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                else
+                    dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
+            }
+        }
+        return dp[n][m];
+    }
+
+    // Longest Common Substring recursion
+//    static int lcs(String s1, String s2, int n, int m, int count) {
+//        if (n == 0 || m == 0) return count;
+//        if (s1.charAt(n - 1) == s2.charAt(m - 1))
+//            count = lcs(s1, s2, n - 1, m - 1, count + 1);
+//        count = Math.max(count,
+//                Math.max(lcs(s1, s2, n, m - 1, 0), lcs(s1, s2, n - 1, m, 0)));
+//        return count;
+//    }
+    // Longest Common Substring dp
+
     // static int max = Integer.MIN_VALUE;
 
 //    public static int lcss(String s1, String s2, int n, int m) {
@@ -83,43 +95,55 @@ public class Longest_common_subsequence {
 //    }
 
     // Longest common subsequence
-    public static String Longest_common_subPrint(String s1, String s2, int n, int m) {
+//    public static String Longest_common_subPrint(String s1, String s2, int n, int m) {
+//
+//        int[][] dp = new int[n + 1][m + 1];
+//        for (int i = 0; i < n + 1; i++) {
+//            for (int j = 0; j < m + 1; j++) {
+//                if (i == 0 || j == 0)
+//                    dp[i][j] = 0;
+//            }
+//
+//        }
+//        for (int i = 1; i < n + 1; i++) {
+//            for (int j = 1; j < m + 1; j++) {
+//
+//                if (s1.charAt(i - 1) == s2.charAt(j - 1))
+//                    dp[i][j] = 1 + dp[i - 1][j - 1];
+//                else
+//                    dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
+//            }
+//        }
+//
+//        int i = n;
+//        int j = m;
+//        StringBuilder s = new StringBuilder();
+//        while (i > 0 && j > 0) {
+//            if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+//                s.append(s1.charAt(i - 1));
+//                i--;
+//                j--;
+//            } else {
+//                if (dp[i][j - 1] > dp[i - 1][j])
+//                    j--;
+//                else
+//                    i--;
+//
+//            }
+//        }
+//        return s.reverse().toString();
+//
+//    }
+//
+//    public static int shortest_common_superSequence(String s1, String s2, int n, int m) {
+//        return n + m - lcs(s1, s2, n, m);
+//    }
 
-        int[][] dp = new int[n + 1][m + 1];
-        for (int i = 0; i < n + 1; i++) {
-            for (int j = 0; j < m + 1; j++) {
-                if (i == 0 || j == 0)
-                    dp[i][j] = 0;
-            }
-
-        }
-        for (int i = 1; i < n + 1; i++) {
-            for (int j = 1; j < m + 1; j++) {
-
-                if (s1.charAt(i - 1) == s2.charAt(j - 1))
-                    dp[i][j] = 1 + dp[i - 1][j - 1];
-                else
-                    dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
-            }
-        }
-
-        int i = n;
-        int j = m;
-        StringBuilder s = new StringBuilder();
-        while (i > 0 && j > 0) {
-            if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
-                s.append(s1.charAt(i - 1));
-                i--;
-                j--;
-            } else {
-                if (dp[i][j - 1] > dp[i - 1][j])
-                    j--;
-                else
-                    i--;
-
-            }
-        }
-        return s.reverse().toString();
-
+    // Minimum Number of Insertion and Deletion to convert String a to String b
+    public static void min_opertation_to_convert_string(String s1, String s2, int n, int m) {
+        int deletion = s1.length() - lcs(s1, s2, n, m);
+        int insertion = s2.length() - lcs(s1, s2, n, m);
+        System.out.println("deletion = " + deletion);
+        System.out.println("insertion = " + insertion);
     }
 }
